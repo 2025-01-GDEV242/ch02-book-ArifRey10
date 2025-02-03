e53814affb44c3ad67cc2be8fe5440a6d1549c3b
ArifRey10 <g00296272@stu.raritanval.edu>
2025-02-03 11:38:19 -0500

    finished the first 3 exercises
    forgot to commit after each exercise

--- Book.java
+++ Book.java
@@ -3,24 +3,40 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author (Arif Reyhan)
+ * @version (2/3/2025)
  */
 class Book
 {
     // The fields.
     private String author;
     private String title;
+    private int pages;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
 
     // Add the methods here ...
+    public String getAuthor()
+    {
+        return author;
+    } 
+    
+    public String getTitle()
+    {
+        return title;
+    } 
+    
+    public int getPages()
+    {
+        return pages;
+    } 
 }
